da33d712d3b99f347e0a9da8c93ae2b43c7075f2
Hildo Bijl <hildobijl@gmail.com>
2024-10-12 11:34:09 +0200

    Put the Firebase config file back

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkUTte8wfL0Qjs8D3IFi9IA_YVx4ANxJk",
+  authDomain: "vierkantescaperoom.firebaseapp.com",
+  projectId: "vierkantescaperoom",
+  storageBucket: "vierkantescaperoom.appspot.com",
+  messagingSenderId: "374748810521",
+  appId: "1:374748810521:web:76f2e44f32bf56c2730988",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)

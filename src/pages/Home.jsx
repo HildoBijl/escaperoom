81c50776291e287b2a33b4c0b1908c2753572503
Hildo Bijl <hildobijl@gmail.com>
2024-10-12 17:27:03 +0200

    Small textual fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,6 @@ import { Subpage } from 'components'
 
 export function Home() {
 	return <Subpage>
-		<p>ToDo: Schrijf een korte tekst dat bezoekers verleidt de Escape Room uit te proberen, en dat kort nog extra info geeft over de doelgroep en mogelijke prijzen.</p>
+		<p>ToDo: Schrijf een korte tekst die bezoekers verleidt de Escape Room uit te proberen, en dat kort nog extra info geeft over de doelgroep en mogelijke prijzen.</p>
 	</Subpage>
 }

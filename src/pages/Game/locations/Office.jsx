0036d749b98fa6274de3166219c24baa0d28d5f9
Hildo Bijl <hildobijl@gmail.com>
2024-10-19 16:56:00 +0200

    Make teacher gender neutral

--- src/pages/Game/locations/Office.jsx
+++ src/pages/Game/locations/Office.jsx
@@ -9,9 +9,9 @@ export function Location({ numVisits, clearHistory }) {
 	if (numVisits === 0) {
 		return <>
 			<ResetButton {...{ clearHistory }} />
-			<p>Tijdens een lange wiskundeles vertel je aan je wiskundedocent dat je wel eens wat andere wiskunde wilt dan de standaard wiskunde van de middelbare school. Je docent krijgt een klein fonkelen in de ogen en neemt je mee naar zijn kantoor achterin het klaslokaal. Uit een grote boekenkast trekt hij een boek over fractals.</p>
+			<p>Tijdens een lange wiskundeles vertel je aan je wiskundedocent dat je wel eens wat andere wiskunde wilt dan de standaard wiskunde van de middelbare school. Je docent krijgt een klein fonkelen in de ogen en neemt je mee naar het kantoor achterin het klaslokaal. Uit een grote boekenkast wordt een boek over fractals tevoorschijn gehaald.</p>
 			<Image src={OfficeImage} />
-			<p>Terwijl de docent terug gaat naar het klaslokaal, ga je zitten in de stoel achter zijn bureau en duik je in de ingewikkelde figuren. Gefascineerd in de patronen vergeet je totaal de tijd. Als je uiteindelijk uit het boek ontwaakt merk je dat het rumoer van het klaslokaal opgehouden is. Een kille stilte lijkt door het gebouw getrokken te zijn. Hoe lang was je wel niet aan het lezen?</p>
+			<p>Terwijl de docent terug gaat naar het klaslokaal, ga je zitten in de stoel achter het grote bureau en duik je in de ingewikkelde figuren. Gefascineerd in de patronen vergeet je totaal de tijd. Als je uiteindelijk uit het boek ontwaakt merk je dat het rumoer van het klaslokaal opgehouden is. Een kille stilte lijkt door het gebouw getrokken te zijn. Hoe lang was je wel niet aan het lezen?</p>
 		</>
 	}
 

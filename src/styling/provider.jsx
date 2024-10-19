f5b64263eee9c4a1a2a9070889c93b03ba058112
Hildo Bijl <hildobijl@gmail.com>
2024-10-19 11:19:33 +0200

    Set default dark mode

--- src/styling/provider.jsx
+++ src/styling/provider.jsx
@@ -5,7 +5,7 @@ import useMediaQuery from '@mui/material/useMediaQuery'
 import { createCustomTheme } from './theme'
 
 export function ThemeProvider({ children }) {
-	const mode = useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light'
+	const mode = useMediaQuery('(prefers-color-scheme: light)') ? 'light' : 'dark' // Default dark mode.
 	const theme = useMemo(() => createCustomTheme({ mode }), [mode])
 
 	return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>

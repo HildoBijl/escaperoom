e0e5ffd5479200e4561ba3d4395174dc146faf6a
Hildo Bijl <hildobijl@gmail.com>
2024-10-19 10:59:47 +0200

    Add Home page together with new image components

---
+++ src/components/Image.jsx
@@ -0,0 +1,3 @@
+export function Image({ src }) {
+	return <img src={src} style={{ display: 'block', margin: '0.5rem auto', maxHeight: '350px' }} />
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
-import { Subpage } from 'components'
+import { Link } from 'react-router-dom'
+
+import { Subpage, Image } from 'components'
+import School from '../assets/School.jpg'
 
 export function Home() {
 	return <Subpage>
-		<p>ToDo: Schrijf een korte tekst die bezoekers verleidt de Escape Room uit te proberen, en dat kort nog extra info geeft over de doelgroep en mogelijke prijzen.</p>
+		<p>Kun jij ontsnappen uit de online wiskundige Escape Room &quot;De verlaten school&quot;? Kijk hoe ver je komt, en win mogelijk een gratis plek voor een zomerkamp!</p>
+		<p style={{ textAlign: 'center' }}><Link to="/spel">Ga naar de Escape Room</Link></p>
+		<Image src={School} />
+		<p>De Escape Room is gericht op scholieren uit de onderbouw van de middelbare school die wel houden van raadsels, getallen, en een beetje uitdaging. Val jij in de doelgroep? En lukt het je om de Escape Room voor [ToDo: deadline] op te lossen? Dan win je mogelijk een gratis plek voor een <Link to="https://www.vierkantvoorwiskunde.nl/">Vierkant voor Wiskunde</Link> zomerkamp! Zie de <Link to="/info">Infopagina</Link> voor details en voorwaarden.</p>
 	</Subpage>
 }

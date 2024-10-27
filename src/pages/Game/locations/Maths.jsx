a18f3eb04fbfad5da8023ef0926edfeccd80933a
Hildo Bijl <hildobijl@gmail.com>
2024-10-27 10:53:33 +0100

    Add info note about the end of the teaser

--- src/pages/Game/locations/Maths.jsx
+++ src/pages/Game/locations/Maths.jsx
@@ -1,7 +1,14 @@
+
+import Alert from '@mui/material/Alert'
+
 export function Location() {
 	return <>
+		<Alert severity="info" sx={{ my: 2 }}>
+			<p style={{ marginTop: 0 }}>Gefeliciteerd! Je bent ontsnapt uit het kantoor! Maar dit was slechts de teaser...</p>
+			<p style={{ marginBottom: 0 }}>De volledige Escape Room is nog in ontwikkeling en komt op <strong>6 januari 2025</strong> online. Vanaf dan kun je ook proberen om uit &quot;De Verlaten School&quot; te ontsnappen. Kom vooral terug!</p>
+		</Alert>
+
 		{/* <p>Het lokaal ziet er exact hetzelfde uit als toen je hem verlaten had. Tegenover je is de deur naar de gang. Links is een tussendeur naar het aardrijkskundelokaal, en rechts kun je door naar natuurkunde.</p> */}
-		<p style={{ fontStyle: 'italic' }}>Dit is voorlopig het einde van de Escape Room. Het wiskundelokaal wordt, net als de lokalen erna, binnenkort toegevoegd. Dus komt snel weer terug!</p>
 	</>
 }
 

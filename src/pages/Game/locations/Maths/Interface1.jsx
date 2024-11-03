a8748ea7b1a0f0978863fd708b6f786f407f9310
Hildo Bijl <hildobijl@gmail.com>
2024-11-03 12:01:20 +0100

    Add util functions for prime factorizations

--- src/pages/Game/locations/Maths/Interface1.jsx
+++ src/pages/Game/locations/Maths/Interface1.jsx
@@ -1,3 +1,9 @@
+import { getFactorization } from 'util'
+
 export function Interface() {
 	return <p>Hier komt de interface voor het kastje.</p>
+}
+
+function getNumPrimeFactors(num) {
+	return getFactorization(num).reduce((sum, value) => sum + value, 0)
 }
--- src/util/numbers.js
+++ src/util/numbers.js
@@ -10,3 +10,42 @@ export function getRandomInteger(min, max, exclude = []) {
 export const easeInOut = x => (x * x) / (2 * (x * x - x) + 1)
 export const easeShift = x => x < 0.5 ? 2 * x ** 2 : 1 - 2 * (1 - x) ** 2
 export const easeShiftSlow = x => x < 0.5 ? 4 * x ** 3 : 1 - 4 * (1 - x) ** 3
+
+// getPrime returns the prime number at the given index.
+const primes = [2, 3, 5, 7, 11]
+export function getPrime(index) {
+	while (index >= primes.length)
+		addPrime()
+	return primes[index]
+}
+function addPrime() {
+	let n = primes[primes.length - 1] + 2
+	while (!isPrime(n))
+		n += 2
+	console.log('Adding', n)
+	primes.push(n)
+}
+
+// isPrime checks if a number is prime.
+export function isPrime(num) {
+	for (let i = 0; getPrime(i) ** 2 <= num; i++) {
+		if (num % getPrime(i) === 0)
+			return false
+	}
+	return true
+}
+
+// getFactorization returns the factorization of a prime number. For 20 = 2^2*3^0*5^1 it returns [2,0,1]. So it only returns an array of powers of prime numbers at the respective index.
+export function getFactorization(num) {
+	const factors = []
+	for (let i = 0; num > 1; i++) {
+		const p = getPrime(i)
+		let count = 0
+		while (num % p === 0) {
+			count++
+			num /= p
+		}
+		factors.push(count)
+	}
+	return factors
+}

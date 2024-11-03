6381c29c4249b8f4ef00687557506cea4b6dfe71
Hildo Bijl <hildobijl@gmail.com>
2024-11-03 18:01:13 +0100

    Fix layout issue where images were too wide on smartphones

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,3 +1,3 @@
 export function Image({ src }) {
-	return <img src={src} style={{ display: 'block', margin: '1rem auto', maxHeight: '350px' }} />
+	return <img src={src} style={{ display: 'block', margin: '1rem auto', maxHeight: '350px', maxWidth: '100%' }} />
 }

2d4633a8f948ee0f6eb4734b3f037d672668ed61
Hildo Bijl <hildobijl@gmail.com>
2024-11-30 12:18:04 +0100

    Add note to solve the escape room together

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import Alert from '@mui/material/Alert'
 
 import { Subpage, Image } from 'components'
 import { OfficeOverview } from 'assets'
@@ -9,5 +10,6 @@ export function Home() {
 		<p style={{ textAlign: 'center' }}><Link to="/spel">Ga naar de Escape Room</Link></p>
 		<Image src={OfficeOverview} />
 		<p>De Escape Room is gericht op scholieren uit de onderbouw van de middelbare school die wel houden van raadsels, getallen, en een beetje uitdaging. Zit jij dit schooljaar in klas 1, 2 of 3 van het voortgezet onderwijs? En lukt het je om de Escape Room voor 13 juni 2025 op te lossen? Dan win je mogelijk een gratis plek voor een <Link to="https://www.vierkantvoorwiskunde.nl/">Vierkant voor Wiskunde</Link> zomerkamp! Zie de <Link to="/info">Infopagina</Link> voor details en voorwaarden.</p>
+		<Alert severity="info">We raden aan om de Escape Room samen te doen. Dat is wel zo leuk! Als elke persoon de Escape Room op een eigen device (laptop, tablet, telefoon) doet, dan kan iedereen los rondlopen en hints zoeken, om vervolgens samen de raadsels op te lossen.</Alert>
 	</Subpage>
 }

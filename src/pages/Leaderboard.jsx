721fa19c1636565eb67e868cf9f98c24b577c29c
Hildo Bijl <hildobijl@gmail.com>
2025-01-12 23:21:15 +0100

    Adjust leaderboard style, fixing it for dark mode

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,5 +1,5 @@
 import { useMemo, Fragment } from 'react'
-import { useTheme, lighten } from '@mui/material/styles'
+import { useTheme, alpha } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 
 import { useCollection } from 'fb'
@@ -15,8 +15,10 @@ export function Leaderboard() {
 function LeaderboardWithData({ leaderboard }) {
 	// Define styling.
 	const theme = useTheme()
-	const headStyle = { background: theme.palette.primary.main, color: theme.palette.primary.contrastText, padding: '4px 8px 6px', borderRadius: '8px' }
-	const fieldStyle = { ...headStyle, background: lighten(theme.palette.primary.main, 0.9), color: '#000', ':nth-of-type(8n+1), :nth-of-type(8n+2), :nth-of-type(8n+3), :nth-of-type(8n+4)': { background: lighten(theme.palette.primary.main, 0.82) } }
+	const fade = alpha
+	const sharedStyle = { background: theme.palette.primary.main, padding: '4px 8px 6px', borderRadius: '8px', display: 'flex', flexFlow: 'row nowrap', alignItems: 'center' }
+	const headStyle = { ...sharedStyle, color: theme.palette.primary.contrastText }
+	const fieldStyle = { ...sharedStyle, fontSize: '0.9em', fontWeight: 500, background: fade(theme.palette.primary.main, 0.18), ':nth-of-type(8n+1), :nth-of-type(8n+2), :nth-of-type(8n+3), :nth-of-type(8n+4)': { background: fade(theme.palette.primary.main, 0.3) } }
 
 	// Process data.
 	const list = useMemo(() => {
@@ -28,14 +30,14 @@ function LeaderboardWithData({ leaderboard }) {
 		<Box sx={{ my: 2, display: 'grid', gridTemplateColumns: '3fr 3fr 1fr 2fr', gap: '3px' }}>
 			<Box sx={{ ...headStyle }}><strong>Naam</strong></Box>
 			<Box sx={{ ...headStyle }}><strong>Plaats</strong></Box>
-			<Box sx={{ ...headStyle, textAlign: 'center' }}><strong>Leeftijd</strong></Box>
-			<Box sx={{ ...headStyle, textAlign: 'center' }}><strong>Opgelost op</strong></Box>
+			<Box sx={{ ...headStyle, justifyContent: 'center' }}><strong>Leeftijd</strong></Box>
+			<Box sx={{ ...headStyle, justifyContent: 'center' }}><strong>Opgelost</strong></Box>
 			{list.map((solver, index) => {
 				return <Fragment key={index}>
 					<Box sx={{ ...fieldStyle }}>{solver.naam}</Box>
 					<Box sx={{ ...fieldStyle }}>{solver.plaats}</Box>
-					<Box sx={{ ...fieldStyle, textAlign: 'center' }}>{solver.leeftijd}</Box>
-					<Box sx={{ ...fieldStyle, textAlign: 'center' }}>{solver.date.toDate().toLocaleDateString('nl')}</Box>
+					<Box sx={{ ...fieldStyle, justifyContent: 'center' }}>{solver.leeftijd}</Box>
+					<Box sx={{ ...fieldStyle, justifyContent: 'center' }}>{solver.date.toDate().toLocaleDateString('nl', { month: "short", day: "numeric" })}</Box>
 				</Fragment>
 			})}
 		</Box>

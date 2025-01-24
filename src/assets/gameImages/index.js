b8896d8a16c891dbf364b5b4f8604928f6715021
Hildo Bijl <hildobijl@gmail.com>
2025-01-24 17:18:26 +0100

    Replace high-res PNG images by low-res JPG images to reduce data traffic

--- src/assets/gameImages/index.js
+++ src/assets/gameImages/index.js
@@ -4,38 +4,38 @@ export { default as UnavailableDoor } from './UnavailableDoor.jpg'
 // Office
 export { default as OfficeOverview } from './OfficeOverview.jpg'
 export { default as OfficeDoor } from './OfficeDoor.jpg'
-export { default as OfficeHint } from './OfficeHint.png'
-export { default as FactorialHint } from './FactorialHint.png'
+export { default as OfficeHint } from './OfficeHint.jpg'
+export { default as FactorialHint } from './FactorialHint.jpg'
 
 // Maths
 export { default as MathsRoom } from './MathsRoom.jpg'
-export { default as MathsHint1 } from './MathsHint1.png'
+export { default as MathsHint1 } from './MathsHint1.jpg'
 export { default as MathsDoor1 } from './MathsDoor1.jpg'
 export { default as MathsDoor2 } from './MathsDoor2.jpg'
-export { default as FractionHint } from './FractionHint.png'
+export { default as FractionHint } from './FractionHint.jpg'
 
 // Music
 export { default as MusicRoom } from './MusicRoom.jpg'
 export { default as BrokenPiano } from './BrokenPiano.jpg'
 export { default as MusicDoor } from './MusicDoor.jpg'
-export { default as MusicHint } from './MusicHint.png'
-export { default as MusicHintAfter } from './MusicHintAfter.png'
-export { default as CypherKey } from './CypherKey.png'
+export { default as MusicHint } from './MusicHint.jpg'
+export { default as MusicHintAfter } from './MusicHintAfter.jpg'
+export { default as CypherKey } from './CypherKey.jpg'
 
 // Art
 export { default as ArtRoom } from './ArtRoom.jpg'
 export { default as ArtInterface } from './ArtInterface.jpg'
-export { default as KeyPainting } from './KeyPainting.png'
+export { default as KeyPainting } from './KeyPainting.jpg'
 
 // History
 export { default as HistoryRoom } from './HistoryRoom.jpg'
 export { default as HistoryDoor } from './HistoryDoor.jpg'
-export { default as FractionHint2 } from './FractionHint2.png'
+export { default as FractionHint2 } from './FractionHint2.jpg'
 
 // Dutch
 export { default as DutchRoom } from './DutchRoom.jpg'
 export { default as DutchInterface } from './DutchInterface.jpg'
-export { default as LanguageHint } from './LanguageHint.png'
+export { default as LanguageHint } from './LanguageHint.jpg'
 
 // Hall
 export { default as HallDoor } from './HallDoor.jpg'
